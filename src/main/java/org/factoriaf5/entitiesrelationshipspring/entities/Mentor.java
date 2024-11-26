daf5e0c4cc34648c89731b561e9d7554baa60f2a
uxoa <palomita@gmail.com>
2024-11-26 19:22:53 +0100

    Feat: Mentor & Patient entities

--- src/main/java/org/factoriaf5/entitiesrelationshipspring/entities/Mentor.java
+++ src/main/java/org/factoriaf5/entitiesrelationshipspring/entities/Mentor.java
@@ -1,4 +1,39 @@
 package org.factoriaf5.entitiesrelationshipspring.entities;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.FilenameFilter;
+
+@Entity
+@Table(name = "Mentors")
 public class Mentor {
+    
+    @Id
+    @Column(name = "idMentor")
+    private Long id;
+    
+    @Column(name = "nameMentor")
+    private String nameMentor;
+    
+    public Mentor(Long id, String nameMentor) {
+        this.id = id;
+        this.nameMentor = nameMentor;
+    }
+    
+    public Mentor() {
+    
+    }
+    
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public String getNameMentor() {
+        return nameMentor;
+    }
 }
--- src/main/java/org/factoriaf5/entitiesrelationshipspring/entities/Patient.java
+++ src/main/java/org/factoriaf5/entitiesrelationshipspring/entities/Patient.java
@@ -1,4 +1,53 @@
 package org.factoriaf5.entitiesrelationshipspring.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Patients")
 public class Patient {
+    
+    
+    @Id
+    @Column(name = "idPatient")
+    private Long id;
+    
+    @Column(name = "namePatient")
+    private String namePatient;
+    
+    @Column(name = "speciePatient")
+    private String speciePatient;
+    
+    @Column(name = "racePatient")
+    private String racePatient;
+    
+    public Patient(Long id, String namePatient, String speciePatient, String racePatient) {
+        this.id = id;
+        this.namePatient = namePatient;
+        this.speciePatient = speciePatient;
+        this.racePatient = racePatient;
+    }
+    
+    public Patient() {
+    
+    }
+    
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public String getNamePatient() {
+        return namePatient;
+    }
+    
+    public String getSpeciePatient() {
+        return speciePatient;
+    }
+    
+    public String getRacePatient() {
+        return racePatient;
+    }
 }
